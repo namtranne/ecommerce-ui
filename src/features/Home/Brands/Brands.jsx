375c1e2065748967c11b59576dee9999fc9af3d6
BuiVuBaoMinh <buivubaominhlqd@gmail.com>
2024-06-23 22:52:14 +0700

    Added Brands component to Homepage

---
+++ src/features/Home/Brands/Brands.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { twMerge } from "tailwind-merge";
+import { GrHpi } from "react-icons/gr";
+import { SiAsus, SiLenovo, SiApple, SiAcer, SiSamsung, SiNvidia, SiMsi } from "react-icons/si";
+
+export const Brands = () => {
+    return (
+    <div className="bg-zinc-900 px-4 py-12 text-zinc-50">
+      <motion.div
+        initial="initial"
+        animate="animate"
+        transition={{
+          staggerChildren: 0.05,
+        }}
+      >
+        <LogoScroller></LogoScroller>
+      </motion.div>
+    </div>
+    );
+}
+
+const LogoScroller = () => {
+  
+    return (
+      <div className="w-full overflow-hidden relative">
+        <ul className="flex text-9xl items-center justify-center md:justify-start space-x-16 animate-infinite-scroll">
+            <li><GrHpi /></li>
+            <li><SiAsus /></li>
+            <li><SiLenovo /></li>
+            <li><SiApple /></li>
+            <li><SiAcer /></li>
+            <li><SiSamsung /></li>
+            <li><SiNvidia /></li>
+            <li><SiMsi /></li>
+            {/* Repeat icons to ensure smooth scrolling */}
+            <li><GrHpi /></li>
+            <li><SiAsus /></li>
+            <li><SiLenovo /></li>
+            <li><SiApple /></li>
+            <li><SiAcer /></li>
+            <li><SiSamsung /></li>
+            <li><SiNvidia /></li>
+            <li><SiMsi /></li>
+        </ul>
+      </div>
+    );
+  };
+
+export default Brands;
