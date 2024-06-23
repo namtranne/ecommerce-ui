a23e2d19b1cd9358329e61bfae1d35b4fe4c4cf9
Lê Tạ Hoàng Chương <hoangchuongleta@gmail.com>
2024-06-23 23:54:12 +0700

    Fix hero overlay

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -22,9 +22,7 @@ function Navbar() {
         <div className="flex-1"></div>
         <nav className="flex py-3">
           <button
-            className={`mx-2 p-2 z-20 font-bold text-2xl ${
-              showModal ? "text-white" : "text-[#99a0ac]"
-            }`}
+            className={`mx-2 p-2 z-20 font-bold text-2xl text-[#99a0ac] hover:text-white`}
           >
             Home
           </button>
@@ -39,62 +37,62 @@ function Navbar() {
               Shop
             </button>
             <div
-              className={`absolute left-1/2 transform -translate-x-1/2 bg-white border border-gray-300 rounded-md transition-opacity duration-500 ease-in-out ${
+              className={`absolute w-3/4 left-1/2 transform -translate-x-1/2 bg-white border border-gray-300 rounded-md transition-opacity duration-500 ease-in-out ${
                 showModal ? "opacity-100 visible" : "opacity-0 invisible"
               }`}
               onMouseEnter={() => setShowModal(true)}
               onMouseLeave={() => setShowModal(false)}
             >
               <div className="flex">
-                <div className="w-1/3 pr-15 m-5">
-                  <text className="block w-full font-bold text-xl pb-5">
+                <div className="w-1/3 pr-15 m-20">
+                  <text className="block w-full font-bold text-3xl pb-5">
                     Categories
                   </text>
-                  <button className="block w-full font-light text-xl text-left pb-2">
+                  <button className="block w-full font-light text-3xl text-left pb-2">
                     Smartphones
                   </button>
-                  <button className="block w-full font-light text-xl text-left pb-2">
+                  <button className="block w-full font-light text-3xl text-left pb-2">
                     Laptops
                   </button>
-                  <button className="block w-full font-light text-xl text-left pb-2">
+                  <button className="block w-full font-light text-3xl text-left pb-2">
                     Tablets
                   </button>
-                  <button className="block w-full font-light text-xl text-left pb-2">
+                  <button className="block w-full font-light text-3xl text-left pb-2">
                     Desktops
                   </button>
-                  <button className="block w-full font-light text-xl text-left pb-2">
+                  <button className="block w-full font-light text-3xl text-left pb-2">
                     Accessories
                   </button>
-                  <button className="block w-full font-light text-xl text-left pb-2">
+                  <button className="block w-full font-light text-3xl text-left pb-2">
                     Keyboards
                   </button>
-                  <button className="block w-full font-light text-xl text-left pb-2">
+                  <button className="block w-full font-light text-3xl text-left pb-2">
                     Mouse
                   </button>
                 </div>
-                <div className="w-1/3 pl-15 m-5">
-                  <text className="block w-full font-bold text-xl pb-5">
+                <div className="w-1/3 pl-15 m-20">
+                  <text className="block w-full font-bold text-3xl pb-5">
                     Brands
                   </text>
-                  <button className="block w-full font-light text-xl text-left pb-2">
+                  <button className="block w-full font-light text-3xl text-left pb-2">
                     Samsung
                   </button>
-                  <button className="block w-full font-light text-xl text-left pb-2">
+                  <button className="block w-full font-light text-3xl text-left pb-2">
                     Apple
                   </button>
-                  <button className="block w-full font-light text-xl text-left pb-2">
+                  <button className="block w-full font-light text-3xl text-left pb-2">
                     Dell
                   </button>
-                  <button className="block w-full font-light text-xl text-left pb-2">
+                  <button className="block w-full font-light text-3xl text-left pb-2">
                     Asus
                   </button>
-                  <button className="block w-full font-light text-xl text-left pb-2">
+                  <button className="block w-full font-light text-3xl text-left pb-2">
                     Acer
                   </button>
-                  <button className="block w-full font-light text-xl text-left pb-2">
+                  <button className="block w-full font-light text-3xl text-left pb-2">
                     MSI
                   </button>
-                  <button className="block w-full font-light text-xl text-left pb-2">
+                  <button className="block w-full font-light text-3xl text-left pb-2">
                     Xiaomi
                   </button>
                 </div>
@@ -110,9 +108,7 @@ function Navbar() {
           </div>
 
           <button
-            className={`mx-2 p-2 z-20 font-bold text-2xl ${
-              showModal ? "text-white" : "text-[#99a0ac]"
-            }`}
+            className={`mx-2 p-2 z-20 font-bold text-2xl text-[#99a0ac] hover:text-white`}
             onClick={scrollToBottom}
           >
             Contact
@@ -120,16 +116,12 @@ function Navbar() {
         </nav>
         <div className="flex-1 flex justify-end">
           <button
-            className={`mx-2 p-2 z-20 font-bold text-2xl ${
-              showModal ? "text-white" : "text-[#99a0ac]"
-            }`}
+            className={`mx-2 p-2 z-20 font-bold text-2xl text-[#99a0ac] hover:text-white`}
           >
             Login
           </button>
           <button
-            className={`mx-2 p-2 z-20 font-bold text-2xl ${
-              showModal ? "text-white" : "text-[#99a0ac]"
-            }`}
+            className={`mx-2 p-2 z-20 font-bold text-2xl text-[#99a0ac] hover:text-white`}
           >
             Register
           </button>
