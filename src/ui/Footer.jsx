ae4941398112fca21ea5c750ae89a55df2ddb97b
Lê Tạ Hoàng Chương <hoangchuongleta@gmail.com>
2024-06-23 23:40:46 +0700

    Header and footer

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,5 +1,74 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhoneSquare, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 function Footer() {
-  return <p>Footer</p>;
+  return (
+    <footer className="relative text-white bg-[#111827] p-4">
+      <div className="absolute top-0 left-0 right-0 transform -translate-y-full">
+        <svg viewBox="0 0 1440 320" className="w-full">
+          <path
+            fill="#111827"
+            fillOpacity="1"
+            d="M0,310L48,304.7C96,299,192,289,288,294C384,299,480,321,576,320.7C672,321,768,299,864,288.7C960,278,1056,278,1152,288.7C1248,299,1344,321,1392,331.3L1440,342L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
+      <p>© 2024 Group 5. All rights reserved.</p>
+      <div className="w-full flex justify-between">
+        <div className="flex w-1/6">
+          <div className="w-1/3">
+            <FontAwesomeIcon icon={faPhoneSquare} />
+            <FontAwesomeIcon icon={faEnvelope} />
+          </div>
+          <div className="w-full">Hotline Email</div>
+          <div className="w-3/4">
+            :<br />:
+          </div>
+          <div className="w-1/3">0123456789 group5@ecommerce.apcs.edu.vn</div>
+        </div>
+        <div className="w-1/4">
+          <text>Get the latest tech news</text>
+          <div className="flex bg-transparent border border-white rounded-lg mt-3 text-lg">
+            <input
+              type="email"
+              placeholder="Your email here"
+              className="flex-grow p-2 rounded-lg bg-transparent text-white focus:outline-none"
+            />
+            <button className="w-min p-2 rounded-r-lg bg-transparent text-white flex items-center justify-center border-l">
+              <FontAwesomeIcon icon={faEnvelope} className="mr-2" /> Subscribe
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center space-x-4 mt-2">
+        <a
+          href="https://www.facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-facebook-f fa-2x"></i>
+        </a>
+        <a
+          href="https://www.instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-instagram fa-2x"></i>
+        </a>
+        <a href="https://x.com" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-twitter fa-2x"></i>
+        </a>
+        <a
+          href="https://www.youtube.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-youtube fa-2x"></i>
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,5 +1,142 @@
+import React, { useState } from "react";
+
 function Navbar() {
-  return <p>Nav bar</p>;
+  const [showModal, setShowModal] = useState(false);
+  const scrollToBottom = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: "smooth",
+    });
+  };
+  return (
+    <>
+      {/* Overlay */}
+      {showModal && (
+        <div className="fixed inset-0 bg-black opacity-60 z-10"></div>
+      )}
+      <header
+        className={`w-full flex justify-between items-center bg-[#212f4d] ${
+          showModal ? "relative z-20" : ""
+        }`}
+      >
+        <div className="flex-1"></div>
+        <nav className="flex py-3">
+          <button
+            className={`mx-2 p-2 z-20 font-bold text-2xl ${
+              showModal ? "text-white" : "text-[#99a0ac]"
+            }`}
+          >
+            Home
+          </button>
+          <div>
+            <button
+              className={`mx-2 p-2 z-20 font-bold text-2xl ${
+                showModal ? "text-white" : "text-[#99a0ac]"
+              }`}
+              onMouseEnter={() => setShowModal(true)}
+              onMouseLeave={() => setShowModal(false)}
+            >
+              Shop
+            </button>
+            <div
+              className={`absolute left-1/2 transform -translate-x-1/2 bg-white border border-gray-300 rounded-md transition-opacity duration-500 ease-in-out ${
+                showModal ? "opacity-100 visible" : "opacity-0 invisible"
+              }`}
+              onMouseEnter={() => setShowModal(true)}
+              onMouseLeave={() => setShowModal(false)}
+            >
+              <div className="flex">
+                <div className="w-1/3 pr-15 m-5">
+                  <text className="block w-full font-bold text-xl pb-5">
+                    Categories
+                  </text>
+                  <button className="block w-full font-light text-xl text-left pb-2">
+                    Smartphones
+                  </button>
+                  <button className="block w-full font-light text-xl text-left pb-2">
+                    Laptops
+                  </button>
+                  <button className="block w-full font-light text-xl text-left pb-2">
+                    Tablets
+                  </button>
+                  <button className="block w-full font-light text-xl text-left pb-2">
+                    Desktops
+                  </button>
+                  <button className="block w-full font-light text-xl text-left pb-2">
+                    Accessories
+                  </button>
+                  <button className="block w-full font-light text-xl text-left pb-2">
+                    Keyboards
+                  </button>
+                  <button className="block w-full font-light text-xl text-left pb-2">
+                    Mouse
+                  </button>
+                </div>
+                <div className="w-1/3 pl-15 m-5">
+                  <text className="block w-full font-bold text-xl pb-5">
+                    Brands
+                  </text>
+                  <button className="block w-full font-light text-xl text-left pb-2">
+                    Samsung
+                  </button>
+                  <button className="block w-full font-light text-xl text-left pb-2">
+                    Apple
+                  </button>
+                  <button className="block w-full font-light text-xl text-left pb-2">
+                    Dell
+                  </button>
+                  <button className="block w-full font-light text-xl text-left pb-2">
+                    Asus
+                  </button>
+                  <button className="block w-full font-light text-xl text-left pb-2">
+                    Acer
+                  </button>
+                  <button className="block w-full font-light text-xl text-left pb-2">
+                    MSI
+                  </button>
+                  <button className="block w-full font-light text-xl text-left pb-2">
+                    Xiaomi
+                  </button>
+                </div>
+                <div className="w-2/3">
+                  <img
+                    className="w-full h-full object-cover object-center rounded-r-md"
+                    src="src/ui/assets/shop-header.jpg"
+                    alt="Shop Header"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <button
+            className={`mx-2 p-2 z-20 font-bold text-2xl ${
+              showModal ? "text-white" : "text-[#99a0ac]"
+            }`}
+            onClick={scrollToBottom}
+          >
+            Contact
+          </button>
+        </nav>
+        <div className="flex-1 flex justify-end">
+          <button
+            className={`mx-2 p-2 z-20 font-bold text-2xl ${
+              showModal ? "text-white" : "text-[#99a0ac]"
+            }`}
+          >
+            Login
+          </button>
+          <button
+            className={`mx-2 p-2 z-20 font-bold text-2xl ${
+              showModal ? "text-white" : "text-[#99a0ac]"
+            }`}
+          >
+            Register
+          </button>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Navbar;
